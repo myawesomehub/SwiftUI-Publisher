ff735c99a9b3e637808ed0c67f9f4892da39ae6f
Mohammad Yasir <mdcode2021@gmail.com>
2024-12-23 15:41:13 +0530

    Created xcode

---
+++ Swifly/Swifly/SwiflyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiflyApp.swift
+//  Swifly
+//
+//  Created by Mohammad Yasir on 23/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiflyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
